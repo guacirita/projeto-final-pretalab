d84a2e7e24805f591ab26ae7a3f84cfe0249ccbc
guacirita <guacirita@gmail.com>
2022-09-28 22:25:21 -0300

    subindo as correções

--- src/Components/Menu/Menu.jsx
+++ src/Components/Menu/Menu.jsx
@@ -7,30 +7,25 @@ function Menu() {
     const [open, setOpen] = useState(false)
 
     function handleMenu() {
-        open ? setOpen(false) : setOpen(tru)
+        open ? setOpen(false) : setOpen(true)
     }
 
     return (
         <nav>
             <List size={32} isOpen={open} onClick={handleMenu} className={styles.hamburguer} />
             <ul className={open ? styles.hamburguerOpen : styles.menu}>
-
                 <li className={open ? styles.itemOpen : styles.item}>
                     <Link className={open ? styles.linkOpen : styles.link} to="/">Home</Link>
                 </li>
-
                 <li className={open ? styles.itemOpen : styles.item}>
                     <Link className={open ? styles.linkOpen : styles.link} to="/about">Sobre</Link>
                 </li>
-
                 <li className={open ? styles.itemOpen : styles.item}>
                     <Link className={open ? styles.linkOpen : styles.link} to="/comments">Comentários</Link>
                 </li>
-
                 <li className={open ? styles.itemOpen : styles.item}>
                     <Link className={open ? styles.linkOpen : styles.link} to="/contact">Contato</Link>
                 </li>
-
                 <li className={open ? styles.itemOpen : styles.item}>
                     <Link className={open ? styles.linkOpen : styles.link} to="/list">Estabelecimentos</Link>
                 </li>
